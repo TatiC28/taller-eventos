762b4bcc5570d75154f65f48773e5bbd12591115
Tati <taticancela28@gmail.com>
2024-08-16 21:31:51 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,11 +1,9 @@
-function mostrarMensaje() {
-    alert('Hola!');
+function mostrarMensaje(e) {
+    e.stopPropagation();
 }
-
 
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('#btnHola');
 
     button.addEventListener('click', mostrarMensaje);
-
 });
